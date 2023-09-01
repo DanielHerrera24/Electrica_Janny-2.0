4994908050e1e021d087933037bbb25f35494cae
Daniel Herrera <info.danielherrera@gmail.com>
2023-08-31 21:31:19 -0700

    Mejora index y uso de js para mostrar

---
+++ js/pages/index.js
@@ -0,0 +1,51 @@
+function index() {
+    const artIndex = document.getElementById("cont-index-principal")
+
+    artIndex.innerHTML = `
+                <article class="artIndex">
+                    <div id="index-info-1" class="div-parrafos">
+                        <p data-aos="fade-down-right" class="index-parrafos-top"><b>Eléctrica
+                                Janny</b> es una empresa dedicada al suministro de
+                            <b>material eléctrico</b> de las principales marcas en San Luis Río Colorado, Sonora.
+                        </p>
+                        <p data-aos="fade-right" class="index-parrafos">Somos profesionales que se
+                            adaptan a las exigencias de tus requerimientos
+                            para satisfacer
+                            completamente tus necesidades, por lo que te atendemos y asesoramos en un horario muy accesible
+                            logrando así que nos visites y aclaremos todas las dudas que tengas.</p>
+                        <p data-aos="fade-up-right" class="index-parrafos">A diferencia de la
+                            competencia, al venir a <b>Eléctrica Janny</b>
+                            encontrarás los mejores productos,
+                            siempre en perfectas condiciones para garantizar la durabilidad de cada material eléctrico que
+                            lleves, así como trabajo pensado y diseñado con los más altos estándares de calidad y atención
+                            personalizada.</p>
+                    </div>
+                    <div id="index-div-img-1" data-aos="zoom-in-left" class="div-imgs">
+                        <img src="./imgs/Tienda-primeraImpresion.jpg" alt="Instalacion de luz en techo" class="index-imgs">
+                    </div>
+                    <div id="index-div-img-2" data-aos="zoom-in-right" class="div-imgs">
+                        <img src="./imgs/Tienda-laMagiaDelFuturo.jpg" alt="Instalacion de spots" class="index-imgs">
+                    </div>
+                    <div id="index-info-2" class="div-parrafos">
+                        <p data-aos="fade-down-left" class="index-parrafos-top">Al visitarnos,
+                            recibirás asesoría integral sobre cada uno de los
+                            servicios
+                            que
+                            tenemos.
+                            Nos mantenemos actualizados respecto a los productos más innovadores del mercado
+                            constantemente.</p>
+                        <p data-aos="fade-left" class="index-parrafos">Elaboramos proyectos que se
+                            adecuan a lo que nos solicites, por lo que
+                            para nosotros trabajar de
+                            la
+                            mano contigo es lo más importante. Juntos lograremos el mejor resultado.
+                        <p data-aos="fade-up-left" class="index-parrafos">Acércate a nuestra
+                            sucursal, visita nuestro <b>sitio oficial</b> y
+                            síguenos en <b>Facebook</b>, no te
+                            pierdas
+                            de todos los beneficios y <b>material eléctrico</b> que tenemos para ti.</p>
+                    </div>
+                </article>`
+}
+
+index()
