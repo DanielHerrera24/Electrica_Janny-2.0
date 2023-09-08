35fd15e97454335fcba632be2e9ecfb668645a80
Daniel Herrera <info.danielherrera@gmail.com>
2023-09-08 00:09:45 -0700

    Textos index responsive

--- js/pages/index.js
+++ js/pages/index.js
@@ -4,16 +4,16 @@ function index() {
     artIndex.innerHTML = `
                 <article class="artIndex">
                     <div id="index-info-1" class="div-parrafos">
-                        <p data-aos="fade-down-right" class="index-parrafos-top"><b>Eléctrica
+                        <p data-aos="fade-down-right" class="index-parrafos-top parrafos"><b>Eléctrica
                                 Janny</b> es una empresa dedicada al suministro de
                             <b>material eléctrico</b> de las principales marcas en San Luis Río Colorado, Sonora.
                         </p>
-                        <p data-aos="fade-right" class="index-parrafos">Somos profesionales que se
+                        <p data-aos="fade-right" class="index-parrafos parrafos">Somos profesionales que se
                             adaptan a las exigencias de tus requerimientos
                             para satisfacer
                             completamente tus necesidades, por lo que te atendemos y asesoramos en un horario muy accesible
                             logrando así que nos visites y aclaremos todas las dudas que tengas.</p>
-                        <p data-aos="fade-up-right" class="index-parrafos">A diferencia de la
+                        <p data-aos="fade-up-right" class="index-parrafos parrafos">A diferencia de la
                             competencia, al venir a <b>Eléctrica Janny</b>
                             encontrarás los mejores productos,
                             siempre en perfectas condiciones para garantizar la durabilidad de cada material eléctrico que
@@ -27,19 +27,19 @@ function index() {
                         <img src="./imgs/Tienda-laMagiaDelFuturo.jpg" alt="Instalacion de spots" class="index-imgs">
                     </div>
                     <div id="index-info-2" class="div-parrafos">
-                        <p data-aos="fade-down-left" class="index-parrafos-top">Al visitarnos,
+                        <p data-aos="fade-down-left" class="index-parrafos-top parrafos">Al visitarnos,
                             recibirás asesoría integral sobre cada uno de los
                             servicios
                             que
                             tenemos.
                             Nos mantenemos actualizados respecto a los productos más innovadores del mercado
                             constantemente.</p>
-                        <p data-aos="fade-left" class="index-parrafos">Elaboramos proyectos que se
+                        <p data-aos="fade-left" class="index-parrafos parrafos">Elaboramos proyectos que se
                             adecuan a lo que nos solicites, por lo que
                             para nosotros trabajar de
                             la
                             mano contigo es lo más importante. Juntos lograremos el mejor resultado.
-                        <p data-aos="fade-up-left" class="index-parrafos">Acércate a nuestra
+                        <p data-aos="fade-up-left" class="index-parrafos parrafos">Acércate a nuestra
                             sucursal, visita nuestro <b>sitio oficial</b> y
                             síguenos en <b>Facebook</b>, no te
                             pierdas
